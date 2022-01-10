6b188e85fb06f15b6caa228d05cff865d30bd7a8
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-10 19:43:05 +0800

    Added bundle

--- build/client/bundle.js
+++ build/client/bundle.js
@@ -17,7 +17,30 @@ var Freaq = function (dataset) {
             if (instance.size() % 2)
                 return nums[half];
             return (nums[half - 1] + nums[half]) / 2.0;
-        }
+        },
+        mode: function () {
+            var arr = instance.dataset;
+            var mode = {};
+            var max = 0, count = 0;
+            for (var i = 0; i < arr.length; i++) {
+                var item = Math.floor(arr[i]);
+                if (mode[item]) {
+                    mode[item]++;
+                }
+                else {
+                    mode[item] = 1;
+                }
+                if (count < mode[item]) {
+                    max = item;
+                    count = mode[item];
+                }
+            }
+            return max;
+        },
+        range: function () { return [
+            instance.dataset.shift(),
+            instance.dataset.pop(),
+        ]; }
     };
     return instance;
 };
