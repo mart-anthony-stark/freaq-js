ae12e0c65169a85f44f71c579c3617e78d88029a
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-11 14:45:22 +0800

    bundled ts

--- build/client/bundle.js
+++ build/client/bundle.js
@@ -1,63 +1,63 @@
 var Freaq = function (dataset) {
+    var size = function () { return instance.dataset.length; };
+    var mean = function () {
+        return instance.summation() / instance.size();
+    };
+    var median = function () {
+        var nums = instance.sort();
+        var half = Math.floor(instance.size() / 2);
+        if (instance.size() % 2)
+            return nums[half];
+        return (nums[half - 1] + nums[half]) / 2.0;
+    };
+    var mode = function () {
+        var arr = instance.dataset;
+        var mode = {};
+        var max = 0, count = 0;
+        for (var i = 0; i < arr.length; i++) {
+            var item = Math.floor(arr[i]);
+            if (mode[item]) {
+                mode[item]++;
+            }
+            else {
+                mode[item] = 1;
+            }
+            if (count < mode[item]) {
+                max = item;
+                count = mode[item];
+            }
+        }
+        return max;
+    };
+    var range = function () {
+        return instance.sort()[instance.dataset.length - 1] - instance.sort()[0];
+    };
     var instance = {
         dataset: dataset,
-        size: function () { return instance.dataset.length; },
+        size: size,
         summation: function () {
             return instance.dataset.reduce(function (prev, current) { return prev + current; }, 0);
         },
         sort: function () {
-            return sort(instance.dataset);
+            return quicksort(instance.dataset);
         },
-        mean: function () {
-            return instance.summation() / instance.size();
-        },
-        median: function () {
-            var nums = instance.sort();
-            var half = Math.floor(instance.size() / 2);
-            if (instance.size() % 2)
-                return nums[half];
-            return (nums[half - 1] + nums[half]) / 2.0;
-        },
-        mode: function () {
-            var arr = instance.dataset;
-            var mode = {};
-            var max = 0, count = 0;
-            for (var i = 0; i < arr.length; i++) {
-                var item = Math.floor(arr[i]);
-                if (mode[item]) {
-                    mode[item]++;
-                }
-                else {
-                    mode[item] = 1;
-                }
-                if (count < mode[item]) {
-                    max = item;
-                    count = mode[item];
-                }
-            }
-            return max;
-        },
-        range: function () { return [
-            instance.dataset.shift(),
-            instance.dataset.pop(),
-        ]; }
+        mean: mean,
+        median: median,
+        mode: mode,
+        range: range
     };
     return instance;
 };
-var sort = function (arr) {
-    if (arr.length <= 1)
-        return arr;
+function quicksort(array) {
+    if (array.length <= 1) {
+        return array;
+    }
+    var pivot = array[0];
     var left = [];
     var right = [];
-    var newArray = [];
-    var pivot = arr.pop();
-    var len = arr.length;
-    for (var i = 0; i < len; i++) {
-        if (arr[i] <= pivot)
-            left.push(arr[i]);
-        else
-            right.push(arr[i]);
+    for (var i = 1; i < array.length; i++) {
+        array[i] < pivot ? left.push(array[i]) : right.push(array[i]);
     }
-    return newArray.concat(sort(left), pivot, sort(right));
-};
+    return quicksort(left).concat(pivot, quicksort(right));
+}
 //# sourceMappingURL=bundle.js.map
